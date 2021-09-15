b202f3bf20513854749b19008885734f43a374d3
Alisson <alissonmendonca2015@gmail.com>
2021-09-15 19:42:39 -0300

    primeiro commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace prova_teste.Controllers
+{
+    public class Conversao
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
